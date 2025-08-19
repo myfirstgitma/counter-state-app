0e8219fa5716684667d769645a0ae8583ebe16ea
Abel Asmelash <abelasmelash07@gmail.com>
2025-08-19 16:23:51 +0000

    regular function to callback function

--- states-hooks/src/components/UseEffectForTitle/UseEffectForTitle.jsx
+++ states-hooks/src/components/UseEffectForTitle/UseEffectForTitle.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import styles from './UseEffectForTitle.module.css'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./UseEffectForTitle.module.css";
 
 const UseEffectForTitle = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    const counterIncreament =()=>{
-    
-    setCount(count + 1)
-}
-useEffect(()=>{
- alert('Component is successfully mounted');
-}, [])
+  useEffect(() => {
+    alert("Component is successfully mounted");
+  }, []);
 
-useEffect(()=>{
-console.log('the count is', count);
-document.title = `count is ${count}`
-}, [count])
+  useEffect(() => {
+    // console.log('the count is', count);
+    document.title = `count is now ${count}`;
+  }, [count]);
 
   return (
     <div>
-    <h2>count is: {count}</h2>
-    <button className={styles.title} onClick={counterIncreament}>start count</button>
+      <h2>count is: {count}</h2>
+      <button
+        className={styles.title}
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        start count
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default UseEffectForTitle
+export default UseEffectForTitle;
